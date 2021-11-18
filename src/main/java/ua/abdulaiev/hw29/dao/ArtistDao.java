5014b2109cfed26266031b610fe3a3fa2778a98d
Ibragim-abdulaev <iabdulaeff6@gmail.com>
2021-11-18 18:41:27 +0200

    Added folder hw29

---
+++ src/main/java/ua/abdulaiev/hw29/dao/ArtistDao.java
@@ -0,0 +1,88 @@
+package ua.abdulaiev.hw29.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import ua.abdulaiev.hw29.models.Artist;
+import ua.abdulaiev.hw29.utils.HibernateUtils;
+import java.util.List;
+
+public class ArtistDao {
+    public void saveArtist(Artist artist) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.saveOrUpdate(artist);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void updateArtist(Artist artist) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(artist);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteArtist(int id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Artist artist = session.get(Artist.class, id);
+            if (artist != null) {
+                session.delete(artist);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public Artist getById(int id) {
+        Transaction transaction = null;
+        Artist artist = null;
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            artist = session.get(Artist.class, id);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return artist;
+    }
+
+    public List<Artist> getAllArtist() {
+        Transaction transaction = null;
+        List<Artist> artists = null;
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            artists = session.
+                    createQuery("SELECT a FROM Artist a", Artist.class)
+                    .list();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return artists;
+    }
+}
---
+++ src/main/java/ua/abdulaiev/hw29/models/Artist.java
@@ -0,0 +1,33 @@
+package ua.abdulaiev.hw29.models;
+import lombok.*;
+import ua.abdulaiev.hw29.enums.ArtistNames;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "artist")
+public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_artist")
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name")
+    private ArtistNames name;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Album album;
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "id=" + id +
+                ", name=" + name +
+                '}';
+    }
+}
